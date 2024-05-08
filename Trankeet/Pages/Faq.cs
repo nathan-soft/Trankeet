ff345ede63cb226249dbbd8adb8bba60ae3c3616
omomowo symeon <omomowosymeon45@gmail.com>
2024-05-08 17:21:44 +0100

    push

---
+++ Trankeet/Pages/Faq.cs
@@ -0,0 +1,10 @@
+﻿namespace TrankeetWeb.Pages
+{
+    public class Faq(string question, string answer, bool isActive = false)
+    {
+        public string Question { get; set; } = question;
+        public string Answer { get; set; } = answer;
+
+        public bool IsActive { get; set; } = isActive;
+    }
+}
